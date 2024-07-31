ded431d734f98c13280f37c11b963b50346c537b
egorkjj <d7085195@gmail.com>
2024-07-31 15:20:15 +0300

    commit

--- tg_bot/DBSM.py
+++ tg_bot/DBSM.py
@@ -221,10 +221,7 @@ def rm_token(token):
 
 Session = sessionmaker()
 session = Session(bind = engine)
-with engine.connect() as conn:
-    conn.execute('ALTER TABLE promos ADD COLUMN users_used JSON NULL')
-    conn.commit()
-    conn.close()
+
 allp = session.query(Promos).all()
 for i in allp:
     session.delete(i)
